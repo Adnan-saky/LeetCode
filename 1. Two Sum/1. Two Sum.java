00f712df4c54046ccc424fc421e0e501897879e6
adnansaky <adnansaky123@gmail.com>
2023-02-02 01:03:47 +0600

    Update 1. Two Sum.java

--- 1. Two Sum/1. Two Sum.java
+++ 1. Two Sum/1. Two Sum.java
@@ -1,5 +1,15 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5};
+        int target = 5;
+        
+
+        System.out.println(twoSum(arr,target));
+    }
+
+    
+    public static int[] twoSum(int[] nums, int target) {
         
         for(int i = 0; i<nums.length ; i++){
             for(int j = i+1 ; j<nums.length ; j++){
@@ -12,4 +22,5 @@ class Solution {
         }
         return null;
     }
+    
 }
